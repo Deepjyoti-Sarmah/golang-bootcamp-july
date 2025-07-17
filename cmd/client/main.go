9cbd713fcc4dd9ddd9b3deb072b0a1232dd5ffde
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2025-07-17 15:33:53 +0530

    added connection to server logic

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,1 +1,72 @@
 package main
+
+import (
+	"context"
+	"golang-bootcamp-July/internal/repository/inmemory"
+	"golang-bootcamp-July/internal/service"
+	"golang-bootcamp-July/internal/worker"
+	"log"
+	"strings"
+	"sync/atomic"
+	"time"
+)
+
+func main() {
+	const (
+		totalTickets  = 50_000
+		totalRequests = 75_000
+		numWorkers    = 1000
+	)
+
+	log.Println("🎫 Ticket Booking System Starting...")
+	log.Printf("📊 Total Tickets Available: %d", totalTickets)
+	log.Printf("👥 Total Booking Requests: %d", totalRequests)
+	log.Printf("⚙️  Worker Pool Size: %d", numWorkers)
+	log.Println(strings.Repeat("-", 50))
+
+	// startTime := time.Now()
+
+	// initialize repository
+	bookingRepo := inmemory.NewBookingRepository()
+	ticketRepo := inmemory.NewTicketRepository(totalTickets)
+
+	bookingService := service.NewBookingService(bookingRepo, ticketRepo)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	workerPool := worker.NewPool(numWorkers, bookingService)
+	workerPool.Start(ctx)
+
+	var (
+		successfulBookings int32
+		failedBookings     int32
+		processedRequests  int32
+	)
+
+	resultDone := make(chan bool)
+
+	go func() {
+		for result := range workerPool.Results() {
+			atomic.AddInt32(&processedRequests, 1)
+
+			if result.Success {
+				atomic.AddInt32(&successfulBookings, 1)
+				if processedRequests%5000 == 0 {
+					log.Printf("✅ Progress: %d/%d requests processed, %d tickets booked",
+						atomic.LoadInt32(&processedRequests),
+						totalRequests,
+						atomic.LoadInt32(&successfulBookings),
+					)
+				} else {
+					atomic.AddInt32(&failedBookings, 1)
+				}
+			}
+			resultDone <- true
+		}
+	}()
+
+	log.Println("🚀 Starting concurrent booking requests...")
+
+	// Client simulation
+}
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,71 @@
 package main
+
+import (
+	"context"
+	"golang-bootcamp-July/internal/handler"
+	"golang-bootcamp-July/internal/repository/inmemory"
+	"golang-bootcamp-July/internal/service"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// initialize repositories
+	bookingRepo := inmemory.NewBookingRepository()
+	ticketRepo := inmemory.NewTicketRepository(50_000)
+
+	// initialize service
+	bookingService := service.NewBookingService(bookingRepo, ticketRepo)
+
+	// initialize handler
+	bookingHandler := handler.NewBookingHandler(bookingService)
+
+	// Setup Gin router
+	r := gin.Default()
+
+	// healthCheck
+	r.GET("/health", bookingHandler.HeathCheck)
+
+	// API routes
+	v1 := r.Group("/api/v1")
+	{
+		v1.POST("/bookings", bookingHandler.BookTicket)
+		v1.GET("/stats", bookingHandler.GetStats)
+		v1.GET("/bookings/user/:userID", bookingHandler.GetUserBookigs)
+	}
+
+	// Server config
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+
+	go func() {
+		log.Println("🚀 Server starting on :8080")
+		if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+			log.Fatalf("Failed to start server: %v", err)
+		}
+	}()
+
+	// Gracefull shutdown
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	log.Println("🛑 Server shutting down...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: %v", err)
+	}
+
+	log.Println("✅ Server exited")
+}
